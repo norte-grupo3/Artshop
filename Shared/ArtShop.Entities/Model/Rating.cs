3f58e18e0266593489a7443e8124b9011d016555
BERNARDO ESPINOZA <b-j-espinoza@hotmail.com>
2020-11-15 12:16:41 -0300

    Rating

--- Shared/ArtShop.Entities/Model/Rating.cs
+++ Shared/ArtShop.Entities/Model/Rating.cs
@@ -5,9 +5,17 @@
 //====================================================================================================
 
 
+using System.ComponentModel.DataAnnotations;
+
 namespace ArtShop.Entities.Model
 {
     public class Rating : IdentityBase
     {
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public int ProductId { get; set; }
+        [Required]
+        public int Stars { get; set; }
     }
 }
