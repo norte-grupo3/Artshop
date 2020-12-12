4b6ed40e63970c4f731e883e0ae449d96ca7484d
Diego Lambrechts <lambrechts.diego@gmail.com>
2020-12-12 14:12:54 -0300

    Actualización de Entidades Cart y Cary Item para que los atributos aparezcan en la API

--- Shared/ArtShop.Entities/Model/Cart.cs
+++ Shared/ArtShop.Entities/Model/Cart.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace ArtShop.Entities.Model
+
 {
     [Serializable]
     public class Cart : IdentityBase
@@ -13,8 +15,11 @@ namespace ArtShop.Entities.Model
         {
             this.CartItem = new HashSet<CartItem>();
         }
+        [DataMember]
         public string Cookie { get; set; }
+        [DataMember]
         public DateTime CartDate { get; set; }
+        [DataMember]
         public int ItemCount { get; set; }
 
         public virtual ICollection<CartItem> CartItem { get; set; }
--- Shared/ArtShop.Entities/Model/CartItem.cs
+++ Shared/ArtShop.Entities/Model/CartItem.cs
@@ -6,15 +6,21 @@
 
 
 using System;
+using System.Runtime.Serialization;
 
 namespace ArtShop.Entities.Model
+
 {
     [Serializable]
     public class CartItem : IdentityBase
     {
+        [DataMember]
         public int CartId { get; set; }
+        [DataMember]
         public int ProductId { get; set; }
+        [DataMember]
         public double Price { get; set; }
+        [DataMember]
         public int Quantity { get; set; }
 
         public virtual Cart Cart { get; set; }
